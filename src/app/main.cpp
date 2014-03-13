df9d414e8b3d2a8dceb80997ebb8e81bf2f14403
Artem Marchenko <artem@thinglink.com>
2014-03-14 01:11:02 +0200

    log to file as well

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -17,6 +17,7 @@
 
 #include "SystemlogAppender"
 #include "Level"
+#include "DailyRollingFileAppender"
 
 #include "qmllogger.h"
 
@@ -40,7 +41,7 @@ void initLogging()
     p_consoleAppender->setName(QLatin1String("root console appender"));
     p_consoleAppender->activateOptions();
 
-    Log4Qt::SystemLogAppender *p_syslogAppender = new Log4Qt::SystemLogAppender(p_layout);
+    Log4Qt::SystemLogAppender *p_syslogAppender = new Log4Qt::SystemLogAppender();
     p_syslogAppender->setServiceName("journalctl");
     p_syslogAppender->setName(QLatin1String("root sysylog appender"));
     p_syslogAppender->setLayout(p_layout);
@@ -51,15 +52,32 @@ void initLogging()
 
     p_syslogAppender->activateOptions();
 
+    Log4Qt::DailyRollingFileAppender *p_fileAppender =
+            new Log4Qt::DailyRollingFileAppender();
+    p_fileAppender->setFile(QStandardPaths::writableLocation(QStandardPaths::CacheLocation)
+                            + "/harbour-log4qtdemo/harbour-log4qtdemo.log");
+    p_fileAppender->setDatePattern(Log4Qt::DailyRollingFileAppender::DAILY_ROLLOVER);
+    p_fileAppender->setLayout(p_layout);
+    p_syslogAppender->setName(QLatin1String("root file appender"));
+    p_fileAppender->activateOptions();
+
 
     // Set appenders on root logger
     Log4Qt::Logger::rootLogger()->addAppender(p_consoleAppender);
     Log4Qt::Logger::rootLogger()->addAppender(p_syslogAppender);
+    Log4Qt::Logger::rootLogger()->addAppender(p_fileAppender);
 
     Log4Qt::Logger::logger(QLatin1String("Main Logger"))->info("Logging started");
 
     bool handingMessages = Log4Qt::LogManager::handleQtMessages();
     qDebug() << "Intercepting messages from qDebug is " << handingMessages;
+    qDebug() << "temp loc" << QStandardPaths::standardLocations(QStandardPaths::TempLocation);
+    qDebug() << "config loc" << QStandardPaths::standardLocations(QStandardPaths::ConfigLocation);
+    qDebug() << "cache loc" << QStandardPaths::standardLocations(QStandardPaths::CacheLocation);
+    qDebug() << "generic cache loc" << QStandardPaths::standardLocations(QStandardPaths::GenericCacheLocation);
+    qDebug() << "data loc" << QStandardPaths::standardLocations(QStandardPaths::DataLocation);
+    qDebug() << "generic data loc" << QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation);
+    qDebug() << "app name is " << QCoreApplication::applicationName();
 }
 
 
